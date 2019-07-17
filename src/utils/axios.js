a71cec066c56e497827da358f13c3d7fb90ea216
sharad-s <sharadshekar@gmail.com>
2019-07-17 10:29:04 -0500

    now

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,6 +4,6 @@ const env = process.env.NODE_ENV;
 const dev = env !== 'production';
 const devPort = parseInt(process.env.PORT, 10) || 3000;
 
-const baseURL = dev  ? `https://9401293c.ngrok.io` : 'https://plug-api-staging.herokuapp.com/';
+const baseURL = dev  ? `http://localhost:${devPort}` : 'https://plug-api-staging.herokuapp.com/';
 
 export default axios.create({ baseURL });
